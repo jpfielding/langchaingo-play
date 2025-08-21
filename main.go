7eae90c77be820b6111fa7a26905406b2ca0d547
JP Fielding <jp.fielding@gmail.com>
2025-08-21 00:29:50 +0000

    chat example

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	_ "github.com/tmc/langchaingo/llms"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
 )
 
 func main() {
+	llm, err := ollama.New(ollama.WithModel("llama3.2"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
 
+	content := []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, "You are a company branding design wizard."),
+		llms.TextParts(llms.ChatMessageTypeHuman, "What would be a good company name a company that makes colorful socks?"),
+	}
+	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Print(string(chunk))
+		return nil
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = completion
 }
